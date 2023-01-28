5b63476b32c8e6805da32d3b3e17ca6164bc7a60
viktor <viktor.nguyen@transwap.com>
2023-01-28 12:09:40 +0700

    feat: sitemap

---
+++ src/pages/sitemap.xml.tsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import axiosWrapper from '~/services/axiosConfig';
+import { CardDichVuModel, Categories } from '~/services/types';
+
+const mainuri = 'https://kangheevietnam.com';
+
+function generateSiteMap(danhMucDichVu: CardDichVuModel[], dichvu: CardDichVuModel[]) {
+  return `<?xml version="1.0" encoding="UTF-8"?>
+   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+     <!--We manually set the two URLs we know already-->
+     <url>
+       <loc>${mainuri}</loc>
+     </url>
+     <url>
+       <loc>${mainuri}/dat-lich</loc>
+     </url>
+     <url>
+       <loc>${mainuri}/dich-vu</loc>
+     </url>
+     <url>
+     <loc>${mainuri}/khuyen-mai</loc>
+   </url>
+   <url>
+   <loc>${mainuri}/lien-he</loc>
+ </url>
+     ${danhMucDichVu
+       .map(({ slug }) => {
+         return `
+       <url>
+           <loc>${mainuri}/dich-vu/${slug}</loc>
+       </url>
+     `;
+       })
+       .join('')}
+       ${dichvu
+         .map(({ slug }) => {
+           return `
+        <url>
+            <loc>${mainuri}/bai-viet/${slug}</loc>
+        </url>
+      `;
+         })
+         .join('')}
+   </urlset>
+ `;
+}
+
+function SiteMap() {}
+export async function getServerSideProps({ res }: any) {
+  const dichvu =
+    (await axiosWrapper
+      .get<CardDichVuModel[]>('/posts', {
+        params: {
+          categories: Categories.DichVu,
+        },
+      })
+      .then(res => res.data)) || [];
+  const dichVuNoiBat =
+    (await axiosWrapper
+      .get<CardDichVuModel[]>('/posts', {
+        params: {
+          categories: Categories.DichVuNoiBat,
+        },
+      })
+      .then(res => res.data)) || [];
+
+  const danhMucDichVu = (await axiosWrapper.get<CardDichVuModel[]>('/danh-muc-dich-vu').then(res => res.data)) || [];
+
+  // We generate the XML sitemap with the posts data
+  const sitemap = generateSiteMap(danhMucDichVu, dichvu);
+
+  res.setHeader('Content-Type', 'text/xml');
+  // we send the XML to the browser
+  res.write(sitemap);
+  res.end();
+
+  return {
+    props: {},
+  };
+}
+
+export default SiteMap;
